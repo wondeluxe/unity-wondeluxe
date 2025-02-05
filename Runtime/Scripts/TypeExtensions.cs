d30b0caaa76b6e44fba7537140e09d9ec4187680
geowal-wondeluxe <geoffrey.walker@wondeluxe.com>
2025-02-06 10:26:02 +1100

    Add GetFieldInHierarchy method to TypeExtensions

--- Runtime/Scripts/TypeExtensions.cs
+++ Runtime/Scripts/TypeExtensions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
 namespace Wondeluxe
 {
@@ -39,6 +40,40 @@ namespace Wondeluxe
 			if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>))
 			{
 				return type.GetGenericArguments()[0];
+			}
+
+			return null;
+		}
+
+		/// <summary>
+		/// Searches for a field, using the specified binding constraints, in a given Type or one of
+		/// its base types.
+		/// </summary>
+		/// <remarks>
+		/// The FlattenHierarchy BindingFlag won't surface private fields in base types, so this 
+		/// method must be used in instances that the field being searched for is or could be private.
+		/// </remarks>
+		/// <param name="type">The Type to search.</param>
+		/// <param name="name">The name of the field to search for.</param>
+		/// <param name="bindingAttr"> bitwise combination of the enumeration values that specify how the search is conducted.</param>
+		/// <returns>An object representing the field that matches the specified requirements, if found; otherwise, <c>null</c>.</returns>
+
+		public static FieldInfo GetFieldInHierarchy(this Type type, string name, BindingFlags bindingAttr)
+		{
+			// Traverse up the type's hierarchy to account for serialized private fields in parent classes.
+
+			Type parentType = type;
+
+			while (parentType != null)
+			{
+				FieldInfo fieldInfo = parentType.GetField(name, bindingAttr);
+
+				if (fieldInfo != null)
+				{
+					return fieldInfo;
+				}
+
+				parentType = parentType.BaseType;
 			}
 
 			return null;
